15dcdaa2563ed83376cf4069d1641c6fc7a6d1eb
Sebastian Moser <sebastian@c2vi.dev>
2024-08-30 08:39:50 +0200

    now use wrs's webview instead of qt

--- src/core/mme.rs
+++ src/core/mme.rs
@@ -31,6 +31,78 @@ impl Mme {
     }
 
     pub fn create_x_window(self) -> MmeResult<()> {
+        use tao::{
+            event::{Event, WindowEvent},
+            event_loop::{ControlFlow, EventLoop},
+            window::WindowBuilder,
+        };
+        use wry::WebViewBuilder;
+
+        fn webui() -> wry::Result<()> {
+            let event_loop = EventLoop::new();
+            let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+            #[cfg(any(
+                target_os = "windows",
+                target_os = "macos",
+                target_os = "ios",
+                target_os = "android"
+            ))]
+            let builder = WebViewBuilder::new(&window);
+
+            #[cfg(not(any(
+                target_os = "windows",
+                target_os = "macos",
+                target_os = "ios",
+                target_os = "android"
+            )))]
+
+            let builder = {
+                use tao::platform::unix::WindowExtUnix;
+                use wry::WebViewBuilderExtUnix;
+                let vbox = window.default_vbox().unwrap();
+                WebViewBuilder::new_gtk(vbox)
+            };
+
+            let _webview = builder
+            //.with_url("file:///home/me/work/mme/test.html")
+            .with_url("file:///home/me/work/mme/presenters/presenters/mme-js/dist/index.html")
+            /*
+            .with_drag_drop_handler(|e| {
+              match e {
+                wry::DragDropEvent::Enter { paths, position } => {
+                  println!("DragEnter: {position:?} {paths:?} ")
+                }
+                wry::DragDropEvent::Over { position } => println!("DragOver: {position:?} "),
+                wry::DragDropEvent::Drop { paths, position } => {
+                  println!("DragDrop: {position:?} {paths:?} ")
+                }
+                wry::DragDropEvent::Leave => println!("DragLeave"),
+                _ => {}
+              }
+
+              true
+            })
+            */
+            .build()?;
+
+            event_loop.run(move |event, _, control_flow| {
+                *control_flow = ControlFlow::Wait;
+
+            if let Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } = event
+            {
+              *control_flow = ControlFlow::Exit
+            }
+            });
+        }
+
+        Ok(webui()?)
+    }
+
+    pub fn create_qt_slot(self) -> MmeResult<()> {
         unsafe {
 
             let backend = i_slint_backend_qt::Backend::new();
@@ -47,7 +119,7 @@ impl Mme {
             //let other_window = OtherWindow::new().unwrap();
             //other_window.show();
 
-            let presenter: Presenter = Presenter::HtmlPresenter(HtmlPresenter::from_folder(Path::new("/home/me/work/mme/mme-presenters/presenters/hello-world").to_owned())?);
+            let presenter: Presenter = Presenter::HtmlPresenter(HtmlPresenter::from_folder(Path::new("/home/me/work/mme/presenters/presenters/hello-world").to_owned())?);
 
             let mut slot: Slot = Slot::QtWidgetSlot(QtWidgetSlot::from_widget(main_widget)?);
 
