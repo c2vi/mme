3492d4ddad39557cd323cf84040e6208cdfada55
Sebastian Moser <sebastian@c2vi.dev>
2024-09-02 19:34:55 +0200

    now use modName

--- mize_module.nix
+++ mize_module.nix
@@ -1,15 +1,42 @@
 {mkMizeRustModule
 , buildModule
 , findModules
+, mkMizeModule
+, buildNpmPackage
 , ...
 }: let
   mmeFlake = builtins.getFlake ./.;
   presenters = mmeFlake.inputs.presenters;
+
+  mkMmePresenter = attrs: mkMizeModule ({
+    select = {
+      inherit (attrs) name;
+      MmePresenter = true;
+    };
+  } // attrs);
+
+  mkMmeNpmPresenter = attrs: buildNpmPackage attrs // {
+  };
+
+  mkMmeHtmlPresenter = attrs: mkMmePresenter {
+    dontUnpack = true;
+    dontPath = true;
+    buildPhase = "";
+    installPhase = ''
+      mkdir -p $out
+      cp -r ${attrs.src}/* $out
+    '';
+  };
+
+  extraArgs = {
+    inherit mkMmePresenter mkMmeHtmlPresenter;
+  };
 in [
 
 mkMizeRustModule {
-  name = "mme";
+  modName = "mme";
   src = ./.;
 }
 
-] ++ map buildModule (findModules presenters)
+# build all presenters as submodules
+] ++ map (mod: buildModule mod extraArgs) (findModules presenters)
